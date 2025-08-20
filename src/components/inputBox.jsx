541e82a355535cf612bfa397b03a803bc6e4635f
ritu jha <ritu05491@gmail.com>
2025-08-20 22:12:06 +0530

    adding components

---
+++ src/components/inputBox.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react'
+
+const inputBox = () => {
+    return (
+        <div className='inputBox bg-white p-4 rounded-lg shadow-md flex flex-row justify-between items-center'>
+            <label htmlFor="from">From:</label>
+            <input type="number" placeholder='0' />
+
+        </div>
+    )
+}
+
+export default inputBox
